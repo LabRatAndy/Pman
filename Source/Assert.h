987426e0f2e24d29d54b78c0dcb3575975c48b07
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2024-11-22 21:14:52 +0000

    Added better logging implementation based on Nathan Baggs youtube video

--- Source/Assert.h
+++ Source/Assert.h
@@ -9,3 +9,55 @@
 #define ASSERT(x, msg)
 #define LOG(msg)
 #endif
+
+#include <print>
+#include <format>
+#include <source_location>
+
+namespace Pman
+{
+	enum class ErrorLevel
+	{
+		TRACE,
+		INFO,
+		WARN,
+		ERROR
+	};
+	template<ErrorLevel L, typename ... Args>
+	struct Log {};
+	template<ErrorLevel L, typename ...Args>
+	struct Log<L,const char*, Args ...>
+	{
+		Log(const char* msg, Args &&... args, std::source_location loc = std::source_location::current())
+		{
+			if constexpr (L == ErrorLevel::TRACE)
+			{
+				std::println("[TRACE] {}:{} {}", loc.file_name(), loc.line(), std::vformat(msg, std::make_format_args(args...)));
+			}
+			else if constexpr (L == ErrorLevel::INFO)
+			{
+				std::println("[INFO] {}:{} {}", loc.file_name(), loc.line(), std::vformat(msg, std::make_format_args(args...)));
+			}
+			else if constexpr (L == ErrorLevel::WARN)
+			{
+				std::println("[WARNING] {}:{} {}", loc.file_name(), loc.line(), std::vformat(msg, std::make_format_args(args...)));
+			}
+			else if constexpr (L == ErrorLevel::ERROR)
+			{
+				std::println("[ERROR] {}:{} {}", loc.file_name(), loc.line(), std::vformat(msg, std::make_format_args(args...)));
+			}
+		}
+	};
+	//Template deduction guide
+	template<ErrorLevel L,typename ...Args>
+	Log(Args ...) -> Log<L, Args ...>;
+
+	template<typename ...Args>
+	using TRACE = Log<ErrorLevel::TRACE, Args ...>;
+	template<typename ...Args>
+	using INFO = Log<ErrorLevel::INFO, Args ...>;
+	template<typename ...Args>
+	using WARN = Log<ErrorLevel::WARN, Args ...>;
+	template<typename ...Args>
+	using ERROR = Log<ErrorLevel::ERROR, Args ...>;
+}
