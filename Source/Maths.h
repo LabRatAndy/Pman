03b939889f6cb27fe2142d4b863483f5e0d73985
LabRatAndy <andrew.dawson@adbioscientist.co.uk>
2025-02-06 20:48:44 +0000

    make  Vec2, 3, and 4 constexpr

--- Source/Maths.h
+++ Source/Maths.h
@@ -13,15 +13,15 @@ namespace Pman
 		T X;
 		T Y;
 
-		Vec2(T x, T y): X(x), Y(y) 
+		constexpr Vec2(T x, T y): X(x), Y(y) 
 		{
 		}
-		void operator=(const Vec2<T>& other)
+		constexpr void operator=(const Vec2<T>& other)
 		{
 			X = other.X;
 			Y = other.Y;
 		}
-		bool operator==(const Vec2<T>& other)
+		constexpr bool operator==(const Vec2<T>& other)
 		{
 			if (X != other.X)
 				return false;
@@ -29,11 +29,11 @@ namespace Pman
 				return false;
 			return true;
 		}
-		bool operator!=(const Vec2<T>& other)
+		constexpr bool operator!=(const Vec2<T>& other)
 		{
 			return !(*this == other);
 		}
-		void ReverseDirection()
+		constexpr void ReverseDirection()
 		{
 			if (X == 1)
 			{
@@ -60,7 +60,7 @@ namespace Pman
 				ASSERT((Y == 0), "This vector is not a direction!");
 			}
 		}
-		bool IsZero() const
+		constexpr bool IsZero() const
 		{
 			if (X == 0 && Y == 0)
 			{
@@ -68,22 +68,22 @@ namespace Pman
 			}
 			return false;
 		}
-		float Length() const
+		constexpr float Length() const
 		{
 			return static_cast<float>(std::hypot(X, Y));
 		}
-		void Normalise()
+		constexpr void Normalise()
 		{
 			float length = this->Length();
 			X = static_cast<T>(X / length);
 			Y = static_cast<T>(Y / length);
 		}
-		void operator*(const int32_t scalar)
+		constexpr void operator*(const int32_t scalar)
 		{
 			X = X * scalar;
 			Y = Y * scalar;
 		}
-		Vec2<T> operator-(const Vec2<T>& other)
+		constexpr Vec2<T> operator-(const Vec2<T>& other)
 		{
 			T x = X - other.X;
 			T y = Y - other.Y;
@@ -99,16 +99,16 @@ namespace Pman
 		T X;
 		T Y;
 		T Z;
-		Vec3(T x, T y, T z) : X(x), Y(y), Z(z)
+		constexpr Vec3(T x, T y, T z) : X(x), Y(y), Z(z)
 		{
 		}
-		void operator=(const Vec3<T>& other)
+		constexpr void operator=(const Vec3<T>& other)
 		{
 			X = other.X;
 			Y = other.Y;
 			Z = other.Z;
 		}
-		bool operator==(const Vec3<T>& other)
+		constexpr bool operator==(const Vec3<T>& other)
 		{
 			if (X != other.X)
 				return false;
@@ -118,7 +118,7 @@ namespace Pman
 				return false;
 			return true;
 		}
-		bool operator!=(const Vec3<T>& other)
+		constexpr bool operator!=(const Vec3<T>& other)
 		{
 			return !(*this == other);
 		}
@@ -130,17 +130,17 @@ namespace Pman
 		T Y;
 		T Z;
 		T W;
-		Vec4(T x, T y, T z, T w) : X(x), Y(y), Z(z), W(w)
+		constexpr Vec4(T x, T y, T z, T w) : X(x), Y(y), Z(z), W(w)
 		{
 		}
-		void operator=(const Vec4<T>& other)
+		constexpr void operator=(const Vec4<T>& other)
 		{
 			X = other.X;
 			Y = other.Y;
 			Z = other.Z;
 			W = other.W;
 		}
-		bool operator==(const Vec4<T>& other)
+		constexpr bool operator==(const Vec4<T>& other)
 		{
 			if (X != other.X)
 				return false;
@@ -152,7 +152,7 @@ namespace Pman
 				return false;
 			return true;
 		}
-		bool operator!=(const Vec4<T>& other)
+		constexpr bool operator!=(const Vec4<T>& other)
 		{
 			return !(*this == other);
 		}
@@ -166,7 +166,7 @@ namespace Pman
 		T Top;
 		T Bottom;
 
-		Rect(T left, T right, T top, T bottom) : Left(left), Right(right), Top(top), Bottom(bottom)
+		constexpr Rect(T left, T right, T top, T bottom) : Left(left), Right(right), Top(top), Bottom(bottom)
 		{
 		}
 	};
